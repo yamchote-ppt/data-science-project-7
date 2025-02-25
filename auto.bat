d1fa92a662a5e5acb883be065148a5dc0709eb8f
Phpahontee Yamchote <yamchote_p@outlook.com>
2025-02-25 14:10:19 +0700

    fetch data on %timestamp%

--- auto.bat
+++ auto.bat
@@ -6,6 +6,7 @@ for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set da
 set timestamp=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2% %datetime:~8,2%:%datetime:~10,2%:%datetime:~12,2%
 
 python fetch_airdata.py
+python read_airdata.py
 git add .
 
 :: Commit with timestamp
